0076ddba5098f6f2e49569da21838cb60f04db24
pedrold10 <pedrogabriel12423@gmail.com>
2020-06-16 16:20:18 -0300

    Adicionando Firebase

---
+++ js/authentication.js
@@ -0,0 +1,21 @@
+
+var authGoogleButton= document.getElementById('authGoogleButton');
+
+function signIn(provider){
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then(function(result){
+            console.log(result);
+            var token=result.credential.acessToken;
+            displayName.innerText = 'Bem vindo, '+ result.user.displayName;
+        }).catch(function(error){
+            console.log(error);
+            alert('Falha na autenticação');
+        });
+}
+
+
+authGoogleButton.addEventListener('click', function(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    signIn(provider);
+});
---
+++ js/config.js
@@ -0,0 +1,13 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appID: "app-id",
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
