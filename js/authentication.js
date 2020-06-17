376cac424e538f3780f31118895dbefd70497210
ManuelMaia586 <57562385+ManuelMaia586@users.noreply.github.com>
2020-06-17 06:02:04 -0700

    Auth firebase

--- js/authentication.js
+++ js/authentication.js
@@ -1,18 +1,21 @@
 
 var authGoogleButton= document.getElementById('authGoogleButton');
 
+
+var displayName = document.getElementById('displayName');
+
 function signIn(provider){
     firebase.auth()
         .signInWithPopup(provider)
         .then(function(result){
             console.log(result);
-            var token=result.credential.acessToken;
+            var token = result.credential.acessToken;
             displayName.innerText = 'Bem vindo, '+ result.user.displayName;
         }).catch(function(error){
             console.log(error);
             alert('Falha na autenticação');
         });
-}
+};
 
 
 authGoogleButton.addEventListener('click', function(){
--- js/config.js
+++ js/config.js
@@ -1,13 +1,17 @@
 // TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "api-key",
-    authDomain: "project-id.firebaseapp.com",
-    databaseURL: "https://project-id.firebaseio.com",
-    projectId: "project-id",
-    storageBucket: "project-id.appspot.com",
-    messagingSenderId: "sender-id",
-    appID: "app-id",
+    apiKey: "AIzaSyDyYl5GGXbPHW8OdQpXPjkUFQiKFKkjEos",
+    authDomain: "bioquiz-11293.firebaseapp.com",
+    databaseURL: "https://bioquiz-11293.firebaseio.com",
+    projectId: "bioquiz-11293",
+    storageBucket: "bioquiz-11293.appspot.com",
+    messagingSenderId: "158238532394",
+    appId: "1:158238532394:web:2a37465a1e335c40363453",
+    measurementId: "G-Z9ZSYS03BB"
   };
-
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  /**/
