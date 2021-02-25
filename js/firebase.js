af9a021bc70255eb8bda2bab767764b9016deabd
WillmaTayanne <willmatayanne@gmail.com>
2021-02-24 22:04:28 -0300

    Login com Facebook

--- js/firebase.js
+++ js/firebase.js
@@ -43,6 +43,26 @@ var db = firebase.firestore();
 
 // LOGIN FACEBOOK
 
+    authFacebookButton.addEventListener('click', function () {
+        // Providers
+        var provider = new firebase.auth.FacebookAuthProvider();
+        signIn(provider);
+    });
+
+    function signIn(provider) {
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then(function (result) {
+                console.log(result);
+                var token = result.credential.accessToken;
+                displayName.innerText = 'Bem vindo, ' + result.user.displayName;
+            }).catch(function (error) {
+                console.log(error);
+                alert('Falha na autenticação');
+            });
+    }
+
+
 // $LOGIN FACEBOOK
 
 // REGISTER
